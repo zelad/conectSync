157023d937b187bd2b21a5599e4049b6e32cb43c
Emrik <rodrigue.beck@gmail.com>
2016-01-24 18:37:51 +0100

    test du fxa-js-client

--- app/js/controller.js
+++ app/js/controller.js
@@ -4,8 +4,10 @@ app.controller('myCtrl', function($scope) {
     $scope.log = "John";
     $scope.mdp = "";
     
-    var Request = require('request');
-    var Hawk = require('hawk');
+//    var Request = require('request');
+//    var Hawk = require('hawk');
+    var client = new FxAccountClient("https://accounts.firefox.com/signin");
+//    var client = new FxAccountClient();
     
- 
+    var promise = client.signIn("rbeck@assystem.com", "comptePourDev");
 });
