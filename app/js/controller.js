889012e2ee1553c727dbbc7e2c6800529290140f
Emrik <rodrigue.beck@gmail.com>
2016-02-13 18:59:24 +0100

    Modif pour tester le projet sur tablette

--- app/js/controller.js
+++ app/js/controller.js
@@ -3,11 +3,21 @@ var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope) {
     $scope.log = "John";
     $scope.mdp = "";
+    $scope.authTimeStamp=0;
+    $scope.logUid="";
     
 //    var Request = require('request');
 //    var Hawk = require('hawk');
     var client = new FxAccountClient("https://api.accounts.firefox.com/v1");
     
     var promise = client.signIn("rbeck@assystem.com", "comptePourDev");
-    console.log(promise);
+//    console.log(promise);
+    promise.then(function(objRx){
+    	console.log("recu: ",objRx);
+    	$scope.authTimeStamp=objRx.authAt;
+    	$scope.logUid=objRx.uid;
+    }, function(reason){
+    	console.log("KO");
+    });
+    
 });
