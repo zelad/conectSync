7ec877595e0bd332eda8e5b660d9a46a5185a7b9
Emrik <rodrigue.beck@gmail.com>
2016-02-21 12:20:24 +0100

    passage du fonctionnel dans la fonction "connectFxA"

--- app/js/connectTool.js
+++ app/js/connectTool.js
@@ -1,38 +1,60 @@
 var connectionManager = {
 		connectFxA: function(scope) {
-			
+		    var client = new FxAccountClient("https://api.accounts.firefox.com/v1");
+		    
+		    var promise = client.signIn("rbeck@assystem.com", "comptePourDev",options={"keys":"true"/*,"reason":"signin"//ne sert pas!*/});
+		    
+//		    console.log(promise);
+		    promise.then(function(objRx){
+		    	console.log("recu: ",objRx);
+		    	
+//		    	var promiseBidAssertion = client.certificateSign(objRx.sessionToken, objRx.uid, 60000);
+//		    	promiseBidAssertion.then(function(objRX){
+//		        	console.log(objRx);
+//		        }, function(reason){
+//		        	console.error("signing KO");
+//		        });
+		    	
+		    	scope.showCredentials(objRx);
+		    }, function(reason){
+		    	console.log("KO");
+		    });
 		}//,
 }
+
+/**
+ * A tester...
+ */
 // Client credentials
-
-    var credentials = {
-        id: 'dh37fgj492je',
-        key: 'werxhqb98rpaxn39848xrunpaw3489ruxnpa98w4rxn',
-        algorithm: 'sha256'
-    }
-
-// Request options
-
-    var requestOptions = {
-        uri: 'http://example.com:8000/resource/1?b=1&a=2',
-        method: 'GET',
-        headers: {}
-    };
-
-// Generate Authorization request header
-
-    var header = Hawk.client.header('http://example.com:8000/resource/1?b=1&a=2', 'GET', { credentials: credentials, ext: 'some-app-data' });
-    requestOptions.headers.Authorization = header.field;
-
-// Send authenticated request
-
-    Request(requestOptions, function (error, response, body) {
-
-    // Authenticate the server's response
-
-        var isValid = Hawk.client.authenticate(response, credentials, header.artifacts, { payload: body });
-
-    // Output results
-
-        console.log(response.statusCode + ': ' + body + (isValid ? ' (valid)' : ' (invalid)'));
-    });
+//
+//    var credentials = {
+//        id: 'dh37fgj492je',
+//        key: 'werxhqb98rpaxn39848xrunpaw3489ruxnpa98w4rxn',
+//        algorithm: 'sha256'
+//    }
+//
+//// Request options
+//
+//    var requestOptions = {
+//        uri: 'http://example.com:8000/resource/1?b=1&a=2',
+//        method: 'GET',
+//        headers: {}
+//    };
+//
+//// Generate Authorization request header
+//
+//    var header = Hawk.client.header('http://example.com:8000/resource/1?b=1&a=2', 'GET', { credentials: credentials, ext: 'some-app-data' });
+//    requestOptions.headers.Authorization = header.field;
+//
+//// Send authenticated request
+//
+//    Request(requestOptions, function (error, response, body) {
+//
+//    // Authenticate the server's response
+//
+//        var isValid = Hawk.client.authenticate(response, credentials, header.artifacts, { payload: body });
+//
+//    // Output results
+//
+//        console.log(response.statusCode + ': ' + body + (isValid ? ' (valid)' : ' (invalid)'));
+//    });
--- app/js/controller.js
+++ app/js/controller.js
@@ -6,27 +6,8 @@ app.controller('myCtrl', function($scope,$timeout) {
     $scope.authTimeStamp=0;
     $scope.logUid="";
     
-//    var Request = require('request');
-//    var Hawk = require('hawk');
-    var client = new FxAccountClient("https://api.accounts.firefox.com/v1");
-    
-    var promise = client.signIn("rbeck@assystem.com", "comptePourDev",options={"keys":"true"/*,"reason":"signin"//ne sert pas!*/});
-    
-//    console.log(promise);
-    promise.then(function(objRx){
-    	console.log("recu: ",objRx);
-    	
-//    	var promiseBidAssertion = client.certificateSign(objRx.sessionToken, objRx.uid, 60000);
-//    	promiseBidAssertion.then(function(objRX){
-//        	console.log(objRx);
-//        }, function(reason){
-//        	console.error("signing KO");
-//        });
-    	
-    	$scope.showCredentials(objRx);
-    }, function(reason){
-    	console.log("KO");
-    });
+//Connection au serveur pour "SignIn"
+    connectionManager.connectFxA($scope);
     
 
     
