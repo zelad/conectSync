28d029ec683c6f9136d723f25c6bd5a98de222ee
Emrik <rodrigue.beck@gmail.com>
2016-02-28 14:54:59 +0100

    test de jsrasign et openpgpjs non concluant nettoyage de "lib"

--- app/js/controller.js
+++ app/js/controller.js
@@ -7,11 +7,38 @@ app.controller('myCtrl', function($scope,$timeout) {
     $scope.logUid="";
     
 //Connection au serveur pour "SignIn"
-    connectionManager.connectFxA($scope);
+//    connectionManager.connectFxA($scope);
     
+//Test de génération de pair de clef en DSA 1024 library "jsrasigne"
+    //https://github.com/kjur/jsrsasign
+//    var rsaPair = KEYUTIL.generateKeypair("RSA", 1024);
+//    var sig = new KJUR.crypto.Signature({"alg": "SHA1withDSA"});
+//    sig.init(rsaPair.prvKeyObj);
+//    sig.updateString('aaa');
+//    var hSigVal = sig.sign();
+    
+//Test de génération de pair de clef en DSA 1024 library "openpgpjs"
+    //https://github.com/openpgpjs/openpgpjs#generate-new-key-pair
+    var openpgp = require('openpgp'); // use as CommonJS, AMD, ES6 module or via window.openpgp
 
+    openpgp.initWorker({ path:'openpgp.worker.js' }) // set the relative web worker path
     
-    /**pour affichage suite à un event*/
+    var options = {
+    	    userIds: [{ name:'Jon Smith', email:'jon@example.com' }], // multiple user IDs
+    	    numBits: 1024,                                            // RSA key size
+    	    passphrase: 'super long and hard to guess secret'         // protects the private key
+    	};
+
+    	openpgp.generateKey(options).then(function(key) {
+    	    var privkey = key.privateKeyArmored; // '-----BEGIN PGP PRIVATE KEY BLOCK ... '
+    	    var pubkey = key.publicKeyArmored;   // '-----BEGIN PGP PUBLIC KEY BLOCK ... '
+    	});
+    
+//    console.info();
+    
+/**
+ * pour affichage suite à un event
+ **/
     $scope.showCredentials = function (objRx) {
     	$timeout(function () {
         	$scope.authTimeStamp=objRx.authAt;
